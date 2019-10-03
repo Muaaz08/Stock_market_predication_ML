7fd02f7aa12e11165f6d67b6788c84f2c6b92f70
Muaaz08 <40697239+Muaaz08@users.noreply.github.com>
2019-10-03 20:25:33 +0530

    Update project.py

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = pd.read_csv(r"C:\Users\inas\Desktop\Machine_learning_project\beancoder_examples-master\linear_reg_article\google.csv")
+data = pd.read_csv(r"google.csv")
 x = np.array(data["Date"])
 x = np.reshape(x, (len(x), 1))
 
